3c773c2f69aa5f9229ad3007c96e1a7b86060027
ftc1718 <1091939302@qq.com>
2015-09-19 15:30:08 +0800

    Maximum Subarray

--- 53.c
+++ 53.c
@@ -1,13 +1,13 @@
-int maxSubArray(int* nums, int numsSize) {
+int maxSubArray(int* nums, int numsSize) {// [-1] >> -1 not [-1] >> 0
     int maxSum, thisSum, i;
 
-    maxSum = *nums;
+    maxSum = *nums; //maxSum = 0;
     thisSum = 0;
     for(i = 0; i < numsSize; i++){
         thisSum += *(nums + i);
         if(thisSum > maxSum)
             maxSum = thisSum;
-        if(thisSum < 0)
+        if(thisSum < 0) // [-3,-2,0,-1]
             thisSum = 0;
     }
     return maxSum;
