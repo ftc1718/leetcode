d4da70ffbbcc929b161389d30f29320385fd4f2a
ftc1718 <1091939302@qq.com>
2015-11-06 23:29:48 +0800

    Path Sum II

---
+++ cpp/113time12ms.cc
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        vector<vector<int>> vec;
+        vector<int> tmp;
+        preOrder(vec, root, tmp, sum);
+        return vec;
+    }
+private:
+    void preOrder(vector<vector<int>> &vec, TreeNode *root, vector<int> &tmp, const int sum)
+    {
+        if(root)
+        {
+            tmp.push_back(root->val);
+            if(root->val == sum && !root->left && !root->right)
+                vec.push_back(tmp);
+            else
+            {
+                preOrder(vec, root->left, tmp, sum - root->val);
+                preOrder(vec, root->right, tmp, sum- root->val);
+            }
+            tmp.pop_back();
+        }
+    }
+};
---
+++ cpp/113time36ms.cc
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        vector<vector<int>> vec;
+        vector<int> tmp;
+        preOrder(vec, root, tmp, sum);
+        return vec;
+    }
+private:
+    void preOrder(vector<vector<int>> &vec, TreeNode *root, vector<int> tmp, const int sum)
+    {
+        if(root)
+        {
+            tmp.push_back(root->val);
+            if(accumulate(tmp.begin(), tmp.end(), 0) == sum && !root->left && !root->right)
+                vec.push_back(tmp);
+            else
+            {
+                preOrder(vec, root->left, tmp, sum);
+                preOrder(vec, root->right, tmp, sum);
+            }
+        }
+    }
+};
