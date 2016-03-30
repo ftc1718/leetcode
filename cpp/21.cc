3673830f1c3a8e9cd3438fec1fd8546aa23970f9
ftc1718 <1091939302@qq.com>
2016-03-30 23:40:01 +0800

    Merge Two Sorted Lists

---
+++ cpp/21.cc
@@ -0,0 +1,78 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1 == NULL) return l2;
+        
+        if(l2 == NULL) return l1;
+        
+//        if(l1 == NULL && l2 == NULL) return NULL;
+        
+        
+        ListNode *head, *cur;
+        
+        if(l1->val < l2->val){
+            cur = l1;
+            l1 = l1->next;
+        }
+        else{
+            cur = l2;
+            l2 = l2->next;
+        }
+        
+        head = cur;
+        
+        while(l1 && l2){
+            if(l1->val < l2->val){
+                cur->next = l1;
+                l1 = l1->next;
+            }
+            else{
+                cur->next = l2;
+                l2 = l2->next;
+            }
+            cur = cur->next;
+            
+        }
+        
+        if(l2) l1 = l2;
+        cur->next = l1;
+        return head;
+    }
+};//12ms
+
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(!l1) return l2;
+        if(!l2) return l1;
+        
+        if(l1->val < l2->val){
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        }
+        else{
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+    }
+};//8ms
